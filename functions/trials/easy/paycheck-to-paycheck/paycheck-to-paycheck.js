8c4949d979dfd724675121aa4ba213207012eb94
RedShucov <redshucov@gmail.com>
2023-05-05 13:28:52 +0300

    Добавит решение испытаний функций лёгкого уровня

---
+++ functions/trials/easy/paycheck-to-paycheck/paycheck-to-paycheck.js
@@ -0,0 +1,28 @@
+// 05.05.23
+// Программа на JS 'От зарплаты до зарплаты', которая:
+// 1. Вычитывает налог из зарплаты и возвращает её без налога.
+
+// Условие:
+// 1. Если зарплата больше 100000, налог =  45%.
+// 2. Если зарплата меньше 100000, налог =  35%.
+
+// Входные данные:
+// Зарплата до вычета - grossSalary{number}
+
+// Выходные данные:
+// Зарплата после вычета  - netSalary{number}
+
+// Сложность:
+// Лёгкая.
+
+const getSalaryWithoutTax = function (grossSalary) {
+  const taxPercent = (grossSalary >= 100000) ? 0.45 : 0.35;
+  const tax = grossSalary * taxPercent;
+  const netSalary = grossSalary - tax;
+  return netSalary;
+}
+
+console.log(getSalaryWithoutTax(100000))
+
+
+
